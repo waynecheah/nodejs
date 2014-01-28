4fd10a35a2e57b87fc5db630fd3ff4ed4bd9d4b1
Wayne Cheah <cheahkokweng@gmail.com>
2014-01-28 18:03:18 +0800

    updating new UI with connectivity status

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
                 port: SERVER_PORT,
                 livereload: LIVERELOAD_PORT,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: '192.168.88.202'
+                hostname: 'localhost'
             },
             livereload: {
                 options: {
